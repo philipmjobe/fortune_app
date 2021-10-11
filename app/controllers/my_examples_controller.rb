f6d72acebfe8bb06d289c9115a03eb430485f106
Philip <philipmjobe@gmail.com>
2021-10-11 13:48:12 -0400

    added fortunes

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,10 @@
 class MyExamplesController < ApplicationController
+  def fortunes
+    array = [
+      "Great riches are coming your way",
+      "All of your wildest dreams will come true",
+      "Immense pain is incoming",
+    ]
+    render html: array.sample
+  end
 end
