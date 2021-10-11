9acb76c1eec221a2742f33871cd80c335726c1be
Philip <philipmjobe@gmail.com>
2021-10-11 13:57:59 -0400

    added third route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/fortunes", controller: "my_examples", action: "fortunes"
   get "/random_nums", controller: "my_examples", action: "random_nums"
+  get "/user_visits", controller: "my_examples", action: "user_visits"
 end
